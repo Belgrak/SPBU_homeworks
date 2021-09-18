6fffc64fabe43328e69523566691fd2d9f0ac1d8
belgrak <glekar03@mail.ru>
2021-09-18 15:16:54 +0300

    Clang formatted

--- homework1/task_2.c
+++ homework1/task_2.c
@@ -1,15 +1,17 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 
-int gettingMaxRepetitiveElement(const int *array, int array_len) {
+int gettingMaxRepetitiveElement(const int* array, int array_len)
+{
     int maxElement = 0;
     bool HasRepetitiveElements = false;
     for (int i = 0; i < array_len; i++) {
         for (int j = i + 1; j < array_len; j++) {
             if (array[i] == array[j]) {
                 HasRepetitiveElements = true;
-                if (array[i] > maxElement) maxElement = array[i];
+                if (array[i] > maxElement)
+                    maxElement = array[i];
             }
         }
     }
@@ -19,11 +21,12 @@ int gettingMaxRepetitiveElement(const int *array, int array_len) {
     return maxElement;
 }
 
-int main() {
+int main()
+{
     int size = 0;
     printf("Введите кол-во элементов в массиве: ");
     scanf("%d", &size);
-    int *array = (int *) calloc(size, sizeof(int));
+    int* array = (int*)calloc(size, sizeof(int));
     for (int i = 0; i < size; i++) {
         printf("Введите число в массив: ");
         scanf("%d", &array[i]);
