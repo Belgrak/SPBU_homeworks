d33634e998a632b44902826dc0a68769f9c35acd
belgrak <glekar03@mail.ru>
2021-09-18 21:32:19 +0300

    correcting mistakes

--- homework1/task_2.c
+++ homework1/task_2.c
@@ -2,36 +2,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int gettingMaxRepetitiveElement(const int* array, int array_len)
+bool existenceOfRepetitiveElements(const int* array, int arrayLen)
+{
+    for (int i = 0; i < arrayLen; i++) {
+        for (int j = i + 1; j < arrayLen; j++) {
+            if (array[i] == array[j]) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+int gettingMaxRepetitiveElement(const int* array, int arrayLen)
 {
     int maxElement = 0;
-    bool HasRepetitiveElements = false;
-    for (int i = 0; i < array_len; i++) {
-        for (int j = i + 1; j < array_len; j++) {
+    for (int i = 0; i < arrayLen; i++) {
+        for (int j = i + 1; j < arrayLen; j++) {
             if (array[i] == array[j]) {
-                HasRepetitiveElements = true;
                 if (array[i] > maxElement)
                     maxElement = array[i];
             }
         }
-    }
-    if (!HasRepetitiveElements) {
-        printf("Error was handed. Array does not have any repetitive elements.\n");
     }
     return maxElement;
 }
 
 int main()
 {
-    int size = 0;
+    int arrSize = 0;
     printf("Введите кол-во элементов в массиве: ");
-    scanf("%d", &size);
-    int* array = (int*)calloc(size, sizeof(int));
-    for (int i = 0; i < size; i++) {
+    scanf("%d", &arrSize);
+    int* array = (int*)calloc(arrSize, sizeof(int));
+    for (int i = 0; i < arrSize; i++) {
         printf("Введите число в массив: ");
         scanf("%d", &array[i]);
     }
-    printf("Max repetitive element: %d", gettingMaxRepetitiveElement(array, size));
+    if (!existenceOfRepetitiveElements(array, arrSize)) {
+        printf("Error was handed. Array does not have any repetitive elements.\n");
+        return 0;
+    }
+    printf("Max repetitive element: %d", gettingMaxRepetitiveElement(array, arrSize));
     free(array);
     return 0;
 }
