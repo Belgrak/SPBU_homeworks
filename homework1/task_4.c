99ca5a04652a3ed030721ee503946010053b1bae
belgrak <glekar03@mail.ru>
2021-09-20 10:12:38 +0300

    corrected mistakes

--- homework1/task_4.c
+++ homework1/task_4.c
@@ -1,30 +1,32 @@
-#include "../library/commonUtils/input_task_1.h"
+#include "../library/commonUtils/ioInputInteger.h"
 #include <stdbool.h>
 #include <stdio.h>
 
 bool isPrime(int number)
 {
     int d = 2;
-    while (d * d <= number && number % d != 0)
-        d++;
-    return d * d > number;
+    while (number > d) {
+        if (number % d == 0) return false;
+        else d++;
+    }
+    return true;
 }
 
 int main()
 {
     printf("Данная программа выводит все простые числа не превосходящие числа a\n");
     int integerA = integerInput('a');
-    bool firstTimeAndSimple = true;
+    bool isPrimeFound = true;
     for (int i = 2; i <= integerA; i++) {
         if (isPrime(i)) {
-            if (firstTimeAndSimple) {
+            if (isPrimeFound) {
                 printf("Простые числа: ");
-                firstTimeAndSimple = false;
+                isPrimeFound = false;
             }
             printf("%d ", i);
         }
     }
-    if (firstTimeAndSimple)
+    if (isPrimeFound)
         printf("К сожалению, простых чисел не превосходящих числа %d нет", integerA);
     return 0;
 }
--- library/commonUtils/input_task_1.h
+++
@@ -1,6 +0,0 @@
-#ifndef SPBGU_INPUT_TASK_1_H
-#define SPBGU_INPUT_TASK_1_H
-
-int integerInput(char nameOfVar);
-
-#endif // SPBGU_INPUT_TASK_1_H
