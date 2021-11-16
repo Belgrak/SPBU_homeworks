a086250a48694b016f721d6366150969999d93c1
Glebus <glekar03@mail.ru>
2021-11-16 11:52:34 +0300

    v 1.0

---
+++ test2/test2.1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "../library/commonUtils/ioInputInteger.h"
+
+long long fib(int n){
+    long long first = 0;
+    long long second = 1;
+    for (int i = 0; i < n - 1; i++){
+        second += first;
+        first = second - first;
+    }
+    return second;
+}
+
+int main()
+{
+    printf("Your Fibonacci number is %lld", fib(integerInput('n')));
+    return 0;
+}
