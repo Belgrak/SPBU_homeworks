90687844b59c8332cf8161e8ccbaa6ea80bce4a5
belgrak <glekar03@mail.ru>
2021-11-16 12:19:47 +0300

    styleguide

--- test2/test2.1.c
+++ test2/test2.1.c
@@ -1,10 +1,11 @@
-#include <stdio.h>
 #include "../library/commonUtils/ioInputInteger.h"
+#include <stdio.h>
 
-long long fib(int n){
+long long fib(int n)
+{
     long long first = 0;
     long long second = 1;
-    for (int i = 0; i < n - 1; i++){
+    for (int i = 0; i < n - 1; i++) {
         second += first;
         first = second - first;
     }
